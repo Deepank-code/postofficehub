5ab8be12e4557da36d99e292bfc0e745d3cf4fd4
Deepank-code <82895981+Deepank-code@users.noreply.github.com>
2025-07-09 18:22:44 +0530

    fixed css

--- app/add-my-scheme/page.js
+++ app/add-my-scheme/page.js
@@ -277,7 +277,10 @@ export default function AddMySchemePage() {
         </div>
 
         {/* Submit Button */}
-        <Button type="submit" className="w-full bg-blue-600 hover:bg-blue-700">
+        <Button
+          type="submit"
+          className="w-full mb-10 bg-blue-600 hover:bg-blue-700"
+        >
           <Clock className="mr-2 h-4 w-4" />
           Add Scheme
         </Button>
