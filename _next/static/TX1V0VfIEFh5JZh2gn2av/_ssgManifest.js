1e7b19d56f9a7aad6948c27fa5a5beb3516603b7
Deepank-code <82895981+Deepank-code@users.noreply.github.com>
2025-07-14 21:46:15 +0530

    Deploy static export

---
+++ _next/static/TX1V0VfIEFh5JZh2gn2av/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[slug]","\u002Fblog\u002Fpage\u002F[page]","\u002Fcalculator\u002F[id]","\u002Fgds-corner\u002Fnotifications\u002F[slug]","\u002Fschemes\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
