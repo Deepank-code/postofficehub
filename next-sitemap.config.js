80b58df7cb44a8300ac5bb0fc95f851a0edf633e
Deepank-code <82895981+Deepank-code@users.noreply.github.com>
2025-07-16 12:09:51 +0530

    Deploy: fresh static build from out folder

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://postofficehub.in",
-  generateRobotsTxt: true, // Automatically generate robots.txt
-  sitemapSize: 5000,
-};
